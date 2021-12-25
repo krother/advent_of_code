cd0f3d545487a2851915d21fdb3fd61489aa4af8
Kristian Rother <krother@academis.eu>
2021-12-25 07:22:34 +0100

    code golf hack

--- 2021/day_25/solution.py
+++ 2021/day_25/solution.py
@@ -6,11 +6,11 @@ https://adventofcode.com/2021/day/25
 import numpy as np
 from itertools import count
 
-EMPTY, CUDOWN, CURIGHT = range(3)
+EMPTY, DOWN, RIGHT = range(3)
 
 
 def cucumber2int(char):
-    return CUDOWN if char == 'v' else CURIGHT if char == '>' else EMPTY
+    return DOWN if char == 'v' else RIGHT if char == '>' else EMPTY
 
 
 def parse(data):
@@ -28,17 +28,14 @@ def move(a, cucumber, axis):
     ori = np.roll(dest, -1, axis=axis)
     a[ori] = EMPTY
     a[dest] = cucumber
+    return dest.sum()
 
 
 def solve(data):
     a = parse(data)
-    prev = a.copy()
     for i in count(1):
-        move(a, CURIGHT, axis=1)
-        move(a, CUDOWN, axis=0)
-        if np.all(a == prev):
+        if move(a, RIGHT, 1) + move(a, DOWN, 0) == 0:
             return i
-        prev = a.copy()
 
 
 if __name__ == '__main__':
