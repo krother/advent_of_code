fd189355a24a7f799a1e2dc8c61347d1b90cd09d
Kristian Rother <krother@academis.eu>
2021-12-26 09:14:39 +0100

    add template for next year

---
+++ template/test_solution.py
@@ -0,0 +1,13 @@
+import pytest
+
+from .solution import solve, solve2
+
+INPUT = """
+"""
+
+
+def test_solve():
+    assert solve(INPUT) == ...
+
+def test_solve2():
+    assert solve2(INPUT) == INPUT
