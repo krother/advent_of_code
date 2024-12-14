211244afe5068430be011875096037dd96c5a841
Kristian Rother <krother@academis.eu>
2024-12-14 06:51:51 +0100

    new helper functions

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -1,3 +1,4 @@
 
 from aoc.priority_queue import PriorityQueue
 from aoc.parsers import parse_numbers, parse_2d_numbers, parse_hash_grid
+from aoc.directions import is_on_grid, DIRECTIONS4
--- aoc/directions.py
+++ aoc/directions.py
@@ -8,3 +8,10 @@ UPRIGHT = 1, -1
 DOWNLEFT = -1, 1
 DOWNRIGHT = 1, 1
 MOVES8 = UP, DOWN, LEFT, RIGHT, UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT
+
+
+DIRECTIONS4 = UP, RIGHT, DOWN, LEFT
+
+def is_on_grid(x, y, grid):
+    """should work for both np arrays and nested lists"""
+    return (x >= 0 and y >= 0 and x < len(grid[0]) and y < len(grid))
