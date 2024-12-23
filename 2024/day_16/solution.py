8c34e1704209c701c995c7870d5a7c207c536e73
Kristian Rother <krother@academis.eu>
2024-12-23 15:52:38 +0100

    WIP

--- 2024/day_16/solution.py
+++ 2024/day_16/solution.py
@@ -1,90 +1,122 @@
 """
-
+Day 16: Reindeer Maze
 
 https://adventofcode.com/2024/day/16
 """
 from aoc import priority_queue, DIRECTIONS4
 from itertools import product
-from copy import copy
 from collections import defaultdict
+
+
+Position = tuple[int, int, int]
 
 
 def parse(data):
     grid = data.strip().split("\n")
     for y, x in product(range(len(grid)), range(len(grid[0]))):
         if grid[y][x] == "S":
-            start = x, y
+            start = x, y, 1
         elif grid[y][x] == "E":
-            end = x, y
+            end = x, y, None
     return grid, start, end
 
 
-def get_next_position(pos, heading):
-    x, y = pos
+def get_next_position(position):
+    x, y, heading = position
     dx, dy = DIRECTIONS4[heading]
-    return x + dx, y + dy
+    return x + dx, y + dy, heading
 
 
-def get_moves(grid, pos, heading, cost):
+def get_moves(grid, pos, cost):
     # move forward
-    nx, ny = get_next_position(pos, heading)
+    nx, ny, heading = get_next_position(pos)
     if grid[ny][nx] != "#":
-        yield (nx, ny), heading, cost + 1
+        yield (nx, ny, heading), cost + 1
     # turn
-    yield pos, (heading + 1) % 4, cost + 1000
-    yield pos, (heading + 3) % 4, cost + 1000
+    x, y, heading = pos
+    yield (x, y, (heading + 1) % 4), cost + 1000
+    yield (x, y, (heading + 3) % 4), cost + 1000
+
+
+class Candidate:
+
+    def __init__(self, pos, cost, path):
+        self.pos = pos
+        self.cost = cost
+        self.path = path
+
+    def __hash__(self):
+        return hash(self.pos)
+
+    def get_move_candidates(self, grid):
+        for newpos, cost in get_moves(grid, self.pos, self.cost):
+            yield Candidate(pos=newpos, cost=cost, path=self.path + [newpos])
+
+
+def find_shortest_path(grid, start, end, expand_end=True, taboo=None, cap=None):
+    if expand_end:
+        ex, ey, _ = end
+        end = set([(ex, ey, h) for h in range(4)])
+    else:
+        end = {end}
+    pq = priority_queue.PriorityQueue()
+    pq.add_task(Candidate(start, 0, [start]), priority=0)
+    visited = set(taboo or [])
+    while pq:
+        c = pq.pop_task()
+        if c.pos not in visited and (cap is None or c.cost <= cap):
+            visited.add(c.pos)
+            if c.pos in end:
+                return c.cost, c.path
+            for newc in c.get_move_candidates(grid):
+                pq.add_task(newc, priority=newc.cost)
+
+    # no path found
+    return None, None
 
 
 def solve(data):
     grid, start, end = parse(data)
-    pq = priority_queue.PriorityQueue()
-    pq.add_task((start, 1, 0), priority=0)
-    visited = set()
-    while pq:
-        pos, heading, cost = pq.pop_task()
-        if (pos, heading) in visited:
-            continue
-        visited.add((pos, heading))
-        if pos == end:
-            return cost
-        for pos, heading, cost in get_moves(grid, pos, heading, cost):
-            pq.add_task((pos, heading, cost), priority=cost)
+    cost, _ = find_shortest_path(grid, start, end)
+    return cost
 
 
 def solve2(data):
     grid, start, end = parse(data)
-    pq = priority_queue.PriorityQueue()
-    pq.add_task((start, 1, 0, start), priority=0)
-    visited = set()
-    path_dict = defaultdict(set)  # all paths leading to a position
-    path_cost = {}  # lowest cost to get to a position
-    best_cost = None
-    while pq:
-        pos, heading, cost, prev = pq.pop_task()
-        path_dict[pos].add(pos)
-        path_dict[pos].update(path_dict[prev])
-        path_cost[pos] = cost
+    main_cost, main_path = find_shortest_path(grid, start, end)
+    result = set(main_path)
+    taboo = set()
+    for x, y, heading in product(range(len(grid[0])), range(len(grid)), range(4)):
+        if grid[y][x] != '#':
+            pivot = x, y, heading
+            print(pivot, "taboo size", len(taboo))
+            if pivot in result:
+                continue
+            cost1, part1 = find_shortest_path(grid, start, pivot, expand_end=False, taboo=taboo, cap=main_cost)
+            if part1:
+                cost2, part2 = find_shortest_path(grid, pivot, end, taboo=taboo, cap=main_cost)
+                if part2 and cost1 + cost2 == main_cost:
+                    # found alternative path
+                    print("found new path")
+                    result.update(part1)
+                    result.update(part2)
+                else:
+                    # pivot element is not on any path
+                    taboo.add(pivot)
+            else:
+                # pivot element is not on any path
+                taboo.add(pivot)
 
-        if (pos, heading) in visited:
-            continue
-        visited.add((pos, heading))
 
-        if pos == end:
-            best_cost = cost
-        elif best_cost and cost > best_cost:
-            return len(path_dict[end])
-        else:
-            for newpos, heading, cost in get_moves(grid, pos, heading, cost):
-                pq.add_task((newpos, heading, cost, pos), priority=cost)
-
-    print(path_dict[end])
-    return len(path_dict[end])
+    result = set((x, y) for x, y, _ in result)
+    return len(result)
 
 
 if __name__ == "__main__":
     input_data = open("input_data.txt").read()
     result = solve(input_data)
-    print(f"Example 1: {result}")  # 105508
+    print(f"Example 1: {result}")
+    assert result == 105508
 
     result = solve2(input_data)
     print(f"Example 2: {result}")  # not 561
